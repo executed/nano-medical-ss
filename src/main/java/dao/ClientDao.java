c72b14036fa9bf63f18a0f35cb8544d45dd00108
Dan Serbyn <denis.serbyn@gmail.com>
2018-11-06 18:41:05 +0200

    Added ClientDao class

--- src/main/java/dao/ClientDao.java
+++ src/main/java/dao/ClientDao.java
@@ -1,16 +1,110 @@
 package dao;
 
+import entity.Client;
+import entity.Client.ClientBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import utility.ClassName;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.UUID;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+import static utility.ClassName.getClassName;
 
 public class ClientDao {
 
     private Dao dao;
     private Connection connection;
 
+    private static final Logger LOGGER = getLogger(getClassName());
+
+    private final String insertQuery = "INSERT INTO client (full_name) VALUES (?);";
+    private final String updateQuery = "UPDATE client SET full_name = ? WHERE id = ?;";
+    private final String selectQuery = "SELECT * FROM client WHERE id = ?;";
+    private final String selectAllQuery = "SELECT * FROM client;";
+    private final String deleteQuery = "DELETE FROM client WHERE id = ?;";
+
     public ClientDao(Dao dao){
         this.dao = dao;
         this.connection = dao.getConnection();
     }
 
+    public void save(Client client){
+        if (client.getId() != null){ update(client); return; }
+        LOGGER.debug("Started saving "+client+" in database.");
+        try {
+            PreparedStatement statement = connection.prepareStatement(insertQuery);
+            statement.setString(1, client.getFullName());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.info("Client " + client + " wasn't saved to database", e);
+        }
+        LOGGER.debug("Client "+client+" saved successfully.");
+    }
 
+    public void update(Client client){
+        if (client.getId() == null){ save(client); return; }
+        LOGGER.debug("Started updating "+client+" in database.");
+        try {
+            PreparedStatement statement = connection.prepareStatement(updateQuery);
+            statement.setString(1, client.getFullName());
+            statement.setObject(2, client.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.info("Client " + client + " wasn't updated in database", e);
+        }
+        LOGGER.debug("Client "+client+" updated successfully.");
+    }
+
+    public Client getById(UUID id){
+        Client client = null;
+        LOGGER.debug("Started getting client by id "+id+" from database.");
+        try {
+            PreparedStatement statement = connection.prepareStatement(selectQuery);
+            statement.setObject(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.first()){
+                client = new ClientBuilder().setId(id)
+                                            .setFullName(resultSet.getString("full_name"))
+                                            .build();
+            }
+        } catch (SQLException e) {
+            LOGGER.info("Client wiht id " + id + " wasn't found in database", e);
+        }
+        LOGGER.debug("Client with id "+id+" get successfully.");
+        return client;
+    }
+
+    public HashSet<Client> getAll(){
+        HashSet<Client> clients = new HashSet<>();
+        LOGGER.debug("Started getting all clients from database.");
+        try {
+            ResultSet resultSet = connection.createStatement().executeQuery(selectAllQuery);
+            while(resultSet.next()){
+                clients.add(new ClientBuilder().setId((UUID) resultSet.getObject("id"))
+                                               .setFullName(resultSet.getString("full_name"))
+                                               .build());
+            }
+        } catch (SQLException e) {
+            LOGGER.info("Process of getting all clients crashed.", e);
+        }
+        LOGGER.debug("All clients get successfully");
+        return clients;
+    }
+
+    public void deleteById(UUID id){
+        LOGGER.debug("Started deleting client with id"+id+" from database.");
+        try {
+            int status = connection.createStatement().executeUpdate(deleteQuery);
+            if (status == 0) LOGGER.debug("No clients were affected while deleting");
+        } catch (SQLException e) {
+            LOGGER.info("Process of deleting client crashed.", e);
+        }
+        LOGGER.debug("Client with id "+id+" deleted successfully");
+    }
 }
--- src/main/java/entity/Client.java
+++ src/main/java/entity/Client.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.util.Objects;
 import java.util.TreeSet;
 import java.util.UUID;
 
@@ -21,19 +22,53 @@ public class Client {
         return timeSlots;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
     public void addTimeSlot(TimeSlot slot){
         this.timeSlots.add(slot);
     }
 
     public void removeTimeSlot(TimeSlot slot){
         this.timeSlots.remove(slot);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client)) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) &&
+                Objects.equals(fullName, client.fullName) &&
+                Objects.equals(timeSlots, client.timeSlots);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, fullName);
+    }
+
+    /**
+     *  Static inner class created as util to work with Client entity.
+     */
+    public static class ClientBuilder {
+
+        private Client client;
+
+        public ClientBuilder(){
+            this.client = new Client();
+        }
+
+        public ClientBuilder(Client client) { this.client = client; }
+
+        public ClientBuilder setId(UUID uuid){
+            this.client.id = uuid;
+            return this;
+        }
+
+        public ClientBuilder setFullName(String fullName){
+            this.client.fullName = fullName;
+            return this;
+        }
+
+        public Client build(){ return this.client; }
     }
 }
